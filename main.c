4c7b2e0389c4fbd80ecd80a73a6bf1da1b89ff4d
TheZ3ro <silvethebest@yahoo.it>
2014-08-26 11:33:33 +0200

    [+] Start Developing

---
+++ main.c
@@ -0,0 +1,41 @@
+/*
+ * main.c
+ * 
+ * Copyright 2014 Freeedom Fighter <insert mail here>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+/* ############## This Bot follow the IRC Standard RFC #############  * 
+ * You can see the complete RFC here:                                 * 
+ * http://tools.ietf.org/html/rfc1459                                 * 
+ *                                                                    * 
+ * ########################## Compilation ##########################  *  
+ * Compile with gcc main.c -o main				                            * 
+ *                                                                    *
+ */
+ 
+ 
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("C-3PO - 0.0.1\n  This software is under the GPL License\n");
+	return 0;
+}
+
