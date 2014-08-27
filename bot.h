c437c7fcee34784e6c6ea7ce6b2a1e68892992e6
TheZ3ro <silvethebest@yahoo.it>
2014-08-28 00:16:20 +0200

    License

--- bot.h
+++ bot.h
@@ -20,8 +20,8 @@
 
 // Semantic Versioning
 #define version_major 0
-#define version_minor 0
-#define version_patch 9
+#define version_minor 1
+#define version_patch 0
 
 // Simple struct with bot configuration ;)
 struct IRC{
